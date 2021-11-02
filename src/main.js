193ef57d45ff1c78ca191a36dae579ec244f6dbe
Marielena <maizaga01@gmail.com>
2021-11-02 01:39:39 -0500

    actulizaciones y funcionalidades

--- src/main.js
+++ src/main.js
@@ -2,25 +2,29 @@ import { example } from './data.js';
 
 import data from './data/ghibli/ghibli.js';
 
-//botones del catálogo
-let btnImagenes = document.getElementsByClassName("portada");
 
+let btnImagenes = document.getElementsByClassName("portada");
+let principalBtn = document.getElementById("btnHeaderPrincipal");
+let pagina2 = document.getElementById("pagina2");
+let principalPage = document.getElementById("principalPage");
+
+//botones del catálogo
 for (let i=0; i< btnImagenes.length; i++) {
     //Añades un evento a cada elemento
     btnImagenes[i].addEventListener("click",function() {
        //Aquí la función que se ejecutará cuando se dispare el evento
-       document.getElementById("princialPage").style.display = "none" ; //En este caso ocultaremos el contenido que se encuentra en "principalPage" al darle click al botón de imagen cliqueado
-       document.getElementById("pagina2").style.display = "block" ;//En este caso mostraremos la página2 al darle click al botón de imagen cliqueado
+       principalPage.style.display = "none" ; //En este caso ocultaremos el contenido que se encuentra en "principalPage" al darle click al botón de imagen cliqueado
+       pagina2.style.display = "block" ;//En este caso mostraremos la página2 al darle click al botón de imagen cliqueado
+       
     });     
 }
 
 //botones del header
-
-let principalBtn = document.getElementById("btnHeaderPrincipal");
 principalBtn.addEventListener("click", function(){
-    document.getElementById("pagina2").style.display = "none";
-    document.getElementById("princialPage").style.display = "block";    
+    pagina2.style.display = "none";
+    principalPage.style.display = "block";    
 });
 
-//
+
+
 console.log(example, data);
