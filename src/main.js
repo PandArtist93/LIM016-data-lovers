e117ba2358c86cfd1420abee0f951c5d771707af
KaterinTello <u.kate.tello@gmail.com>
2021-11-16 00:33:46 -0500

    se añadieron cambios en js, existe funcionalidad en orden, filtrado y boton de scroll top

--- src/main.js
+++ src/main.js
@@ -561,10 +561,12 @@ btnScrollTop.addEventListener("click", function() {
     //         behavior: "smooth";
     //   });
       
-    setTimeout(function(){
-        window.scrollTo({
+    window.scrollTo({
             top: 0,
             behavior: "auto"
-        });
-    },0);
+    })
 });
+
+
+//filtra en función de la pelicula seleccionada-----
+
