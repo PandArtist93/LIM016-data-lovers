1c97463b04953569518965d4b069a447e1f86261
Marielena <maizaga01@gmail.com>
2021-11-18 08:01:02 -0500

    Updates

--- data.js
+++ data.js
@@ -1,9 +1,121 @@
 // estas funciones son de ejemplo
+import data from './data/ghibli/ghibli.js';
+const films = data.films;
 
-export const example = () => {
-  return 'example';
+//-----------------------------FILTER----------------------------------------
+//---------------------------filter by director----------------------------
+export const directorFilter = (director) => {       
+  return films.filter((film) => {
+    return film.director ===  director;
+  });    
 };
 
-export const anotherExample = () => {
-  return 'OMG';
+//---------------------------filter by producer----------------------------
+export const producerFilter = (producer) => {
+  return films.filter((film) => {
+    return film.producer ===  producer;
+  });
 };
+
+//---------------------------filter by movie----------------------------
+export const moviesFilter = (movieId) => {
+  return films.filter((film) => {
+    return film.id === movieId;
+  });
+};
+
+//---------------------------filter by release date----------------------------
+export const  yearFilter = (release_date) => {
+  return films.filter((film) => {
+    return film.release_date === release_date;
+  });
+}
+
+//-----------------------------ORDER-----------------------------------
+//---------------------order alfabético de la a--z ---------------------
+export const orderAz = (films) => {      
+  return films.sort((a,b) => {
+    if (a.title < b.title) {
+      return -1;
+    }
+    if (a.title > b.title) {
+      return 1;
+    }
+    return 0;
+  }); 
+    
+}
+
+//---------------------order alfabético de la z--a ---------------------
+export const orderZa = (films) => {      
+  return films.sort((a,b) => {
+    if (a.title > b.title) {
+      return -1;
+    }
+    if (a.title < b.title) {
+      return 1;
+    }
+    return 0;
+  }); 
+    
+}
+
+// ----------orden por fecha de lanzamiento (mas antigua)-----------
+export const orderYearAsc = (films) => {      
+  return films.sort((a,b) => {
+    if (a.release_date < b.release_date) {
+        return -1;
+    }
+    if (a.release_date > b.release_date) {
+        return 1;
+    }
+    return 0;
+  });
+    
+}
+
+// ----------orden por fecha de lanzamiento (mas nueva)-----------
+export const orderYearDesc= (films) => {
+  return films.sort((a,b) => {
+    if (a.release_date > b.release_date) {
+        return -1;
+    }
+    if (a.release_date < b.release_date) {
+        return 1;
+    }
+    return 0;
+  });
+}
+
+//----------orden por calificación (Mayor puntaje)-----------
+export const mayorPuntaje = (films) => {
+  return films.sort((a,b) => {
+    if (parseInt(a.rt_score) < parseInt(b.rt_score)) {
+        return -1;
+    }
+    if (parseInt(a.rt_score) > parseInt(b.rt_score)) {
+        return 1;
+    } 
+    return 0;
+  });
+  
+};
+
+//----------orden por calificación (menor puntaje)-----------
+export const menorPuntaje = (films) => {
+  return films.sort((a,b) => {
+    if (parseInt(a.rt_score) < parseInt(b.rt_score)) {
+        return -1;
+    }
+    if (parseInt(a.rt_score) > parseInt(b.rt_score)) {
+        return 1;
+    } 
+    return 0;
+  });
+  
+};
+
+
+
+
+
--- main.js
+++ main.js
@@ -1,8 +1,16 @@
-// import { conditionalExpression } from '@babel/types';
-/* import { example } from './data.js'; */
+/* import { conditionalExpression } from '@babel/types'; */
 
+import { directorFilter } from './data.js';
+import { moviesFilter } from './data.js';
+import { producerFilter } from './data.js';
+import { yearFilter } from './data.js';
+import { orderAz } from './data.js';
+import { orderZa } from './data.js';
+import { orderYearAsc } from './data.js';
+import { orderYearDesc } from './data.js';
+import { mayorPuntaje } from './data.js';
+import { menorPuntaje } from './data.js';
 import data from './data/ghibli/ghibli.js';
-
 const films = data.films;
 
 // ---------------------modal------------------------
@@ -71,10 +79,10 @@ function renderCharacter(character){
                 <p>Eye color: ${character.eye_color}</p><br>
                 <p>Hair color: ${character.hair_color}</p><br>
                 <p>Specie: ${character.specie}</p>        
-            </div>
-            <div class="codigoQR">
-                <img src="./imagenes/codigoQR.PNG">
-            </div>
+            </div> 
+            <div class = "link">
+                <a href="https://www.studioghibli.com.au/"> Oficial Page of Studio Ghibli </a>
+            </div>           
         </div>
     `
     let modalDataContainer = document.getElementById("modalData");
@@ -95,10 +103,10 @@ function renderLocation(location){
                 <p>Climate: ${location.climate}</p><br>
                 <p>Terrain: ${location.terrain}</p><br>
                 <p>Surface water: ${location.surface_water}</p><br>    
-            </div>
-            <div class="codigoQR">
-                <img src="./imagenes/codigoQR.PNG">
-            </div>
+            </div>  
+            <div>
+                <a href="https://www.studioghibli.com.au/">Oficial Page of Studio Ghibli</a>
+            </div>          
         </div>
     `
     let modalDataContainer = document.getElementById("modalData");
@@ -106,8 +114,7 @@ function renderLocation(location){
 }
 
 //renderizar contenido del modal al seleccioar vehículos
-function renderVehicle(vehicle){
-    /* console.log(vehicle); */
+function renderVehicle(vehicle){    
     let modalData =`
         <div class="fondoImg">
             <div class="imagen-carnet" id="imagen-carnet">          
@@ -122,8 +129,8 @@ function renderVehicle(vehicle){
                 <p>length: ${vehicle.length}</p><br>    
                 <p>pilot name:${vehicle.pilot.name}</p><br> 
             </div>
-            <div class="codigoQR">
-                <img src="./imagenes/codigoQR.PNG">
+            <div>
+                <a href="https://www.studioghibli.com.au/">Oficial Page of Studio Ghibli</a>
             </div>
         </div>
     `
@@ -133,7 +140,6 @@ function renderVehicle(vehicle){
 
 //renderizar información detallada de la película en la 2da vista
 function renderMovieDetail(movie) {
-
     let locations;
     let characters;
     let vehicles;
@@ -143,12 +149,16 @@ function renderMovieDetail(movie) {
     let contenidoPageLocaciones = document.getElementById("contenidoPageLocaciones");
     let contenidoPageVehiculos = document.getElementById("contenidoPageVehiculos");
 
+    contenidoPagePersonajes.innerHTML = "";
+    contenidoPageLocaciones.innerHTML = "";
+    contenidoPageVehiculos.innerHTML = "";
+
     // generar lo datos basicos de la película selecionada
     let contentMovie =`
         <div class="contentImagPage2" id="textPage6"><img src="${movie.poster}"></div>
         <div class="contentMovie">
-            <div class="contentPage2" id="textpage2">Título:${movie.title}</div>
-            <div class="contentPage2" id="textpage3">Sinopsis: ${movie.description} </div>
+            <div class="contentPage2" id="textpage2">${movie.title}</div>
+            <div class="contentPage2" id="textpage3">${movie.description} </div>
             <div class="contentPage2" id="textpage4">Director: ${movie.director}</div>
             <div class="contentPage2" id="textpage5">Productor:  ${movie.producer}</div>
             <div class="contentPage2" id="textpage7">Fecha de lanzamiento: ${movie.release_date} </div>
@@ -192,14 +202,13 @@ function renderMovieDetail(movie) {
 //apertura la 2da vista y llama a la función que rendereiza el contenido de la pelicula seleccionada 
 function addMovieCallbacks(btnMovies){
     let pagina2 =  document.getElementById("pagina2");
-    let principalPage = document.getElementById("principalPage");
-
+    let principalPage = document.getElementById("principalPage");    
     for(let i=0; i < btnMovies.length; i++){
         btnMovies[i].addEventListener("click", function() {            
             /* console.log(btnMovies[i].id); */ // encontramos el id de la pelicula seleccionada
             let movie = films.filter( (film) => {
-                return film.id === btnMovies[i].id                
-;           });
+                return film.id === btnMovies[i].id;
+            });
             principalPage.style.display = "none" ; 
             pagina2.style.display = "block" ;
             renderMovieDetail(movie[0]);
@@ -249,9 +258,7 @@ function addVehicleCallback(vehicleDivs, vehicles) {
             modal.classList.toggle("modalClose");
             let vehicleData = vehicles.filter((vehicle) => {
                 return vehicle.id === vehicleDivs[i].id
-            });
-            /* console.log(vehicleDivs[i].id); */
-            /* console.log(vehicleData); */
+            });            
             renderVehicle(vehicleData[0]);
         });
     }
@@ -296,12 +303,80 @@ function createVehicle(vehicle) {
     return nuevoElemento;
 }
 
-// ---------------------------------boton inicio dle menu principal--------------
+// ---------------------------------boton inicio del menu principal--------------
 let principalBtn = document.getElementById("btnHeaderPrincipal");
+let pagina2 = document.getElementById("pagina2");
+let principalPage = document.getElementById("principalPage");
 principalBtn.addEventListener("click", function(){    
     pagina2.style.display = "none";
+    pagina3.style.display = "none";
+    pagina4.style.display = "none";
     principalPage.style.display = "block";  
     location.reload();    
+});
+
+// ---------------------------------boton peliculas del menu principal---------------
+function renderMovieDropdown() {
+    let listaOcultaSegundaria = document.getElementById("listaOcultaSegundaria");
+    let liElement = "";
+    for(let i=0; i < films.length; i++){
+        liElement += `<li><a id="${films[i].id}">${films[i].title}</a></li>`
+    }
+    listaOcultaSegundaria.innerHTML = liElement;
+    addMoviesFilterCallback();
+}
+
+function addMoviesFilterCallback(){
+    let movies = document.getElementById("listaOcultaSegundaria").children;
+    for(let i=0; i < movies.length; i++){
+        movies[i].firstChild.addEventListener("click", function(e){
+            principalPage.style.display = "none";
+            pagina2.style.display = "block";
+            renderMovieDetail((moviesFilter(e.target.id)[0]));
+        });
+    }
+}
+
+/* function moviesFilter(movieId){
+    let filteredMovies = films.filter((film) => {
+        return film.id === movieId ;
+    });
+    return filteredMovies
+} */
+//----------------------------------boton géneros del menu principal------------
+let pagina3 = document.getElementById("pagina3");
+let generosBtn = document.getElementById("btnHeaderGeneros").children;
+for(let i=0; i < generosBtn.length; i++){
+    generosBtn[i].firstChild.addEventListener("click", function(){
+        pagina5.style.display = "none";   
+        pagina4.style.display = "none";
+        pagina3.style.display = "block";
+        pagina2.style.display = "none";
+        principalPage.style.display = "none";  
+    });
+}
+
+//----------------------------------boton nosotros del menu principal------------
+let pagina4 = document.getElementById("pagina4");
+let nosotrosBtn = document.getElementById("btnHeaderNosotros");
+nosotrosBtn.addEventListener("click", function(){  
+    pagina5.style.display = "none";  
+    pagina4.style.display = "block";
+    pagina3.style.display = "none";
+    pagina2.style.display = "none";
+    principalPage.style.display = "none";      
+});
+
+//----------------------------------boton gráficas del menu principal------------
+let pagina5 = document.getElementById("pagina5");
+let graficasBtn = document.getElementById("btnGraficas");
+graficasBtn.addEventListener("click", function(){   
+    pagina5.style.display = "block";
+    pagina4.style.display = "none"; 
+    pagina3.style.display = "none";
+    pagina4.style.display = "none";
+    pagina2.style.display = "none";
+    principalPage.style.display = "none";      
 });
 
 //----------------------------------render principal-------------------------------
@@ -311,17 +386,18 @@ function render(films) {
     addMovieCallbacks(btnPortadasDivs);  
 }
 
+
 renderDirectorDropdown();
 renderProducerDropdown();
 renderYearDropdown();
 render(films);
+renderMovieDropdown();
 
 // -------------------------------filters-----------------------------------------
 
 // ----------genera el dorpdown de directores-----------
 function renderDirectorDropdown() {
-    let listDirector = document.getElementById("listDirector");
-    //let directors = [... new Set(films.map(x => x.director))];
+    let listDirector = document.getElementById("listDirector");    
     let uniqueDirectors = films.map(x => x.director).filter(
         (director, index, directors) => directors.indexOf(director) === index
     );
@@ -335,24 +411,20 @@ function renderDirectorDropdown() {
 
 //filtra en función del director seleccionado 
 function addDirectorFilterCallback(){
-    let directors = document.getElementById("listDirector").children;
-    //let boxPoster = document.getElementById("boxPoster");
+    let directors = document.getElementById("listDirector").children;    
     for(let i=0; i < directors.length; i++){
-        directors[i].firstChild.addEventListener("click", function(e){
-            //console.log(directorFilter(e.target.id));
-            //renderMovies(directorFilter(e.target.id), boxPoster);
+        directors[i].firstChild.addEventListener("click", function(e){            
             render(directorFilter(e.target.id));
         });
     }
 }
 
-function directorFilter(director) {
-    //console.log(director);
+/* function directorFilter(director) {    
     let filteredMovies = films.filter((film) => {
         return film.director ===  director;
     })
     return filteredMovies
-}
+}*/
 
 // ---------------generar dropdows productores------------
 function renderProducerDropdown() {
@@ -378,12 +450,13 @@ function addProducerFilterCallback(){
     }
 }
 
-function producerFilter(producer) {
+/* function producerFilter(producer) {
     let filteredMovies = films.filter((film) => {
         return film.producer ===  producer;
     });
     return filteredMovies
-}
+} */
+
 
 // ---------------generar dropdows por Año------------
 function renderYearDropdown() {
@@ -408,117 +481,91 @@ function addYearFilterCallback(){
     }
 }
 
-function yearFilter(release_date){
+/* function yearFilter(release_date){
     let filteredYears = films.filter((film) => {
         return film.release_date === release_date;
     });
     return filteredYears
-}
+} */
 
 // -------------------------------orderns-----------------------------------------
 // ----------orden de a--z -----------
 
 let a_z = document.getElementById("a_z");
-a_z.addEventListener("click", function(){       
-    
-    let peliculasOrdenadasPorTitulo = films.sort((a,b) => {
-        if (a.title < b.title) {
-            return -1;
-        }
-        if (a.title > b.title) {
-            return 1;
-        }
-        return 0;
-    });
-    render (peliculasOrdenadasPorTitulo);
+a_z.addEventListener("click", function(){    
+    render (orderAz(films));     
         
 });   
 
 // ----------orden de z--a -----------
 let z_a = document.getElementById("z_a");
 z_a.addEventListener("click", function(){
-    
-    let peliculasOrdenadasPorTitulo = films.sort((a,b) => {
-        if (a.title > b.title) {
-            return -1;
-        }
-        if (a.title < b.title) {
-            return 1;
-        }
-        return 0;
-    });
-    render (peliculasOrdenadasPorTitulo);
-
+    render (orderZa(films));
+     
 });
 
 // ----------orden por fecha de lanzamiento (mas antigua)-----------
 let yearAsc = document.getElementById("yearAsc");
 yearAsc.addEventListener("click", function(){
-  
-    let peliculasOrdenadasPorYear = films.sort((a,b) => {
-        if (a.release_date < b.release_date) {
-            return -1;
-        }
-        if (a.release_date > b.release_date) {
-            return 1;
-        }
-        return 0;
-    });
-
-    render (peliculasOrdenadasPorYear);  
-
+    render (orderYearAsc(films));   
+   
 });
 
 // ----------orden por fecha de lanzamiento (mas nueva)-----------
 let yearDesc = document.getElementById("yearDesc");
 yearDesc.addEventListener("click", function(){
-  
-    let peliculasOrdenadasPorYear = films.sort((a,b) => {
-        if (a.release_date > b.release_date) {
-            return -1;
-        }
-        if (a.release_date < b.release_date) {
-            return 1;
-        }
-        return 0;
-    });
-
-    render (peliculasOrdenadasPorYear);  
-
+    render (orderYearDesc (films)); 
 });
 
 // ----------orden por calificación (mayor puntaje)-----------
 let mayorToMenor = document.getElementById("mayorToMenor");
 mayorToMenor.addEventListener("click", function(){
-    
-    let peliculasOrdenadasPorcalificacion = films.sort((a,b) => {
-        if (parseInt(a.rt_score) > parseInt(b.rt_score)) {
-            return -1;
-        }
-        if (parseInt(a.rt_score) < parseInt(b.rt_score)) {
-            return 1;
-        }
-        return 0;
-    });
-
-    render (peliculasOrdenadasPorcalificacion);
+    render (mayorPuntaje(films));
 });
 
 // ----------orden por calificación (menor puntaje)-----------
 let menorToMayor = document.getElementById("menorToMayor");
-menorToMayor.addEventListener("click", function(){
-    
-    let peliculasOrdenadasPorcalificacion = films.sort((a,b) => {
-        if (parseInt(a.rt_score) < parseInt(b.rt_score)) {
-            return -1;
-        }
-        if (parseInt(a.rt_score) > parseInt(b.rt_score)) {
-            return 1;
-        }
-        return 0;
-    });
-    
-    render (peliculasOrdenadasPorcalificacion);
+menorToMayor.addEventListener("click", function(){       
+    render (menorPuntaje(films));
+      
 });
 
-//-------------------------------------------------
+// ---------------------------creando div para el boton de top scroll-----------------------------
+let buttomScrollTop = document.getElementById("buttomScrollTop")
+
+let creationButtomScrollTop="";
+creationButtomScrollTop +=`<button id="btnScrollTop" class="btnScrollTop"><i class="fas fa-arrow-circle-up"></i></button>`     
+
+buttomScrollTop.innerHTML=creationButtomScrollTop;
+
+// -------------------------- boton de top scroll--------------------------------------------------
+
+let btnScrollTop = document.getElementById("btnScrollTop");
+document.addEventListener("scroll", handleScroll);
+function handleScroll() {     // do something on scroll
+  var scrollableHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var GOLDEN_RATIO = 0.7;
+  if ((document.documentElement.scrollTop / scrollableHeight ) > GOLDEN_RATIO) {    
+    btnScrollTop.style.display = "block";   //show button
+  } else {    
+    btnScrollTop.style.display = "none";    //hide button
+  }
+}
+
+btnScrollTop.addEventListener("click", function() {      
+    window.scrollTo({
+            top: 0,
+            behavior: "auto"
+    })
+});
+
+//------------------------barra de busqueda-------------------------------
+let cardFilter =document.getElementById("cardFilter");
+cardFilter.addEventListener("keyup", e => {    
+    let resultSearch = films.filter((film) => {
+        return film.title.toLowerCase().includes(e.target.value.toLowerCase());
+    });       
+    render(resultSearch);
+});
+
+//------------------------
